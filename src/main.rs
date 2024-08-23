04d765ee4537b6b1f96d3bb14a67cb2a75c8262b
Pedro Rocha <pedrojrocha@outlook.com>
2024-08-23 22:54:42 +0200

    Take and give ownership zigzag

--- src/main.rs
+++ src/main.rs
@@ -222,14 +222,36 @@ fn heap_memory_test() {
     let s3 = s2.clone();
     println!("s2 = {}, s3 = {}", s2, s3);
 
-    ownership_test(s3);
+    // Take ownership test
+    take_ownership(s3);
     println!("s2 = {}", s2);
     // println!("s3 = {}", s3);   ---> this will not compile, s3 was moved to ownership_test
+
+    // Give ownership test
+    let s3 = give_ownership();
+    println!("s3 = {}", s3);
+
+    // Take and give back ownership test
+    let s4 = String::from("hello");
+    let s5 = take_and_give_back_ownership(s4);
+    // println!("s4 = {}", s4);   ---> this will not compile, s4 was moved to s5
+    println!("s5 = {}", s5);
 }
 
-// Ownership test
-fn ownership_test(input_string: String) {
+// Take ownership test
+fn take_ownership(input_string: String) {
     println!("{}", input_string);
+}
+
+// Give ownership test
+fn give_ownership() -> String {
+    let s = String::from("hello");
+    s
+}
+
+// Takes and gives back ownership
+fn take_and_give_back_ownership(input_string: String) -> String {
+    input_string
 }
 
 // Control the flow
