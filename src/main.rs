f1c16a3290d04ea469675e30820317c855b280d7
Pedro Rocha <pedrojrocha@outlook.com>
2024-08-23 10:37:14 +0200

    Mutability

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+
+    // Mutability check
+    mutability();
+}
+
+// Experiment with mutability
+fn mutability() {
+    let mut x = 5; // mutable variable
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let appels = 5; // immutable variable
+    let mut bananas = 5; // mutable variable
+    println!("appels: {}", appels);
+    println!("bananas: {}", bananas);
+
+    // appels = 6; // this would not compile
+    bananas = 6;
+    println!("appels: {}", appels);
+    println!("bananas: {}", bananas);
 }
