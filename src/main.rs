32447a21f5667bc99694ee1fbd6a2c08700fdf4a
Pedro Rocha <pedrojrocha@outlook.com>
2024-08-23 19:42:50 +0200

    The loop

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,10 @@ fn main() {
     let result = function_return_value(5);
     println!("The result is: {}", result);
 
+    // Control the flow
+    println!("** Control the flow **");
+    let_it_flow(23);
+
     // Guess a number game
     println!("** Guess a number **");
     guess_a_number();
@@ -198,4 +202,55 @@ fn function_return_value(x: i32) -> i32 {
 // The simplest function
 fn my_lucky_number() -> i32 {
     23
+}
+
+// Control the flow
+fn let_it_flow(x: i32) {
+    // if statement
+    if x > 10 {
+        println!("x is greater than 10");
+    } else {
+        println!("x is less than or equal to 10");
+    }
+
+    if x != 0 {
+        println!("x is not zero");
+    }
+
+    if x % 2 == 0 {
+        println!("x is even");
+    } else {
+        println!("x is odd");
+    }
+
+    // loop statement
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            println!("Counter reached 10");
+            break counter * 2;
+        }
+    };
+    println!("The result is: {}", result);
+
+    // while statement
+    let mut count_down = 10;
+    while count_down != 0 {
+        println!("{}!", count_down);
+        count_down -= 1;
+    }
+    println!("Goooooo!!!");
+
+    // for statement
+    let weeks = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    for weekday in weeks {
+        println!("{}", weekday);
+    }
+
+    // Countdown using for statement
+    for number in (1..10).rev() {
+        println!("{}!", number);
+    }
 }
