aad3bfd8e929dc5a348dbbeb3c05e750e084df1c
Pedro Rocha <pedrojrocha@outlook.com>
2024-08-24 15:19:07 +0200

    More var ownership tests

--- src/main.rs
+++ src/main.rs
@@ -255,6 +255,15 @@ fn heap_memory_test() {
     let s5 = take_and_give_back_ownership(s4);
     // println!("s4 = {}", s4);   ---> this will not compile, s4 was moved to s5
     println!("s5 = {}", s5);
+
+    // Use s5 string without taking ownership, by passing a reference (a read-only reference)
+    let length = calculate_length(&s5);
+    println!("The length of '{}' is: {}", s5, length);
+
+    // Now pass a mutable reference, to modify the string
+    let mut s6 = String::from("hello");
+    modify_string(&mut s6);
+    println!("s6 = {}", s6);
 }
 
 // Take ownership test
@@ -271,6 +280,16 @@ fn give_ownership() -> String {
 // Takes and gives back ownership
 fn take_and_give_back_ownership(input_string: String) -> String {
     input_string
+}
+
+// Calculate the length of a string (without taking ownership of the input string, like it happens with take_ownership example)
+fn calculate_length(input_string: &String) -> usize {
+    input_string.len()
+}
+
+// Modify the content of a string using a mutable reference
+fn modify_string(input_string: &mut String) {
+    input_string.push_str(" world!");
 }
 
 // Control the flow
